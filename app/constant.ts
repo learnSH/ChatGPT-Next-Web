cf00b113568ae9a821893d56916a275b7f406a37
shi <shishaopeng@gohigh.com.cn>
2023-04-16 07:15:28 +0800

    地址

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,6 @@
 export const OWNER = "learnSH";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://www.houfaka.com/links/590032FF`;
+export const REPO_URL = `https://m.tb.cn/h.UG1XLc0?tk=waqkdlh70OS`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
